dd20c88f139ad64b3613ddc90a0333285ce76937
gellaur <131149021+gellaur@users.noreply.github.com>
2025-03-02 20:23:20 +0700

    Create style.js

---
+++ style.js
@@ -0,0 +1,38 @@
+// Toggle animasi muncul/hilangnya biodata
+function toggleBio() {
+    let bio = document.getElementById("bioSection");
+    if (bio.classList.contains("show")) {
+        bio.classList.remove("show");
+    } else {
+        bio.classList.add("show");
+    }
+}
+
+// Galeri Foto dengan Navigasi Next & Previous
+let imageIndex = 0;
+let imageSources = [
+    "https://nayiella.github.io/image/photo-1.jpg",
+    "https://nayiella.github.io/image/photo-2.jpg",
+    "https://nayiella.github.io/image/photo-3.jpg",
+    "https://nayiella.github.io/image/photo-4.jpg",
+    "https://nayiella.github.io/image/photo-5.jpg",
+    "https://nayiella.github.io/image/photo-6.jpg"
+];
+
+function updateGallery() {
+    document.getElementById("galleryImage").src = imageSources[imageIndex];
+}
+
+function prevImage() {
+    imageIndex = (imageIndex - 1 + imageSources.length) % imageSources.length;
+    updateGallery();
+}
+
+function nextImage() {
+    imageIndex = (imageIndex + 1) % imageSources.length;
+    updateGallery();
+}
+
+window.onload = function () {
+    updateImage(); 
+};
